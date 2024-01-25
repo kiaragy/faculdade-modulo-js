1d6a4a0413a86fa9af32a3cf3cfca0860835bff7
kiaragy <147684406+kiaragy@users.noreply.github.com>
2024-01-25 02:02:04 -0300

    feat: first commit

---
+++ modulo1.js
@@ -0,0 +1,4 @@
+let nome ="ana silva"
+let idade = "30"
+
+console.log ("seu nome e: " + nome + "  sua  idade e: " + idade) 
---
+++ script2.js
@@ -0,0 +1,3 @@
+let rlsync = require('readline-sync');
+let nome = rlsync.question("qual o seu nome?\n")
+console.log(`Olá, ${nome}`)
